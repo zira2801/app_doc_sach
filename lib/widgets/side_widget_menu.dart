b878061834dd4aaf3730141eed9d5d56162e09ef
nhatlamvominh13 <130225186+nhatlamvominh13@users.noreply.github.com>
2024-06-27 15:08:05 +0700

    nhat bi gay

--- lib/widgets/side_widget_menu.dart
+++ lib/widgets/side_widget_menu.dart
@@ -2,55 +2,108 @@ import 'package:app_doc_sach/data/side_menu_data.dart';
 import 'package:flutter/material.dart';
 
 class SideWidgetMenu extends StatelessWidget {
-  const SideWidgetMenu({Key? key}) : super(key: key);
+  const SideWidgetMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          const DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.blue,
-            ),
-            child: Text(
-              'Menu',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
+      child: Container(
+        color: const Color(0xFFD8EFD3),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+              child: Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.3),
+                          blurRadius: 10,
+                          offset: const Offset(0, 5),
+                        ),
+                      ],
+                    ),
+                    child: const Image(
+                      image: AssetImage('assets/icon/logoapp.png'),
+                      height: 50,
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  const Flexible(
+                    child: Text(
+                      'Admin',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 45,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-          //...:nó chèn all phần tử của 1 danh sách vào một list khác
-          ...SideMenuData().menu.map((menuItem) {
-            return ListTile(
-              leading: Icon(menuItem.icon),
-              title: Text(menuItem.title),
-              onTap: () {
-                Navigator.pop(context); // Close the drawer
-                switch (menuItem.title) {
-                  case 'Dashboard':
-                    Navigator.pushNamed(context, '/homepage');
-                    break;
-                  case 'Book':
-                    Navigator.pushNamed(context, '/bookpage');
-                    break;
-                  case 'Category':
-                    Navigator.pushNamed(context, '/category');
-                    break;
-                  case 'Author':
-                    Navigator.pushNamed(context, '/author');
-                    break;
-                  case 'User':
-                    Navigator.pushNamed(context, '/user');
-                    break;
-                  // Add other cases for different menu items here
-                }
-              },
-            );
-          }).toList(),
-        ],
+            const Divider(color: Colors.grey),
+            ...SideMenuData().menu.map((menuItem) {
+              return Column(
+                children: [
+                  ListTile(
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                    title: Row(
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Icon(
+                            menuItem.icon,
+                            color: Colors.black,
+                            size: 30, // Increased size of Icon
+                          ),
+                        ),
+                        const SizedBox(width: 20), // Increased width for spacing
+                        Expanded(
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              menuItem.title,
+                              style: const TextStyle(
+                                fontSize: 24, // Increased font size for Text
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    onTap: () {
+                      Navigator.pop(context); // Close the drawer
+                      switch (menuItem.title) {
+                        case 'Dashboard':
+                          Navigator.pushNamed(context, '/homepage');
+                          break;
+                        case 'Book':
+                          Navigator.pushNamed(context, '/bookpage');
+                          break;
+                        case 'Category':
+                          Navigator.pushNamed(context, '/category');
+                          break;
+                        case 'Author':
+                          Navigator.pushNamed(context, '/author');
+                          break;
+                        case 'User':
+                          Navigator.pushNamed(context, '/user');
+                          break;
+                      // Add other cases for different menu items here
+                      }
+                    },
+                  ),
+                  const Divider(color: Colors.grey), // Add a divider between items
+                ],
+              );
+            }).toList(),
+          ],
+        ),
       ),
     );
   }
